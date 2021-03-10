25af4ab68a7530471d54fd43b2c08db9ad54142d
Bastien Ricard <bastien.ricard-dev@outlook.com>
2021-03-10 12:06:50 +0100

    Add KNP & Faker bundle and refactoring template

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -5,16 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PostFixtures extends Fixture
 {
+    protected $faker;
+
     public function load(ObjectManager $manager)
     {
+        $this->faker = Factory::create();
+
         for ($i = 0; $i < 6; $i++) {
             $post = (new Post)
-                ->setTitle('Hello')
-                ->setContent("This is my first post");
-
+                ->setTitle($this->faker->sentence($nbWords = 6, $variableNbWords = true))
+                ->setContent($this->faker->text());
+            ;
             $manager->persist($post);
         }
         $manager->flush();
