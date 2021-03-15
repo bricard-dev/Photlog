28719e9217d3511f4a92f6bc6f8d442075defcff
Bastien Ricard <bastien.ricard-dev@outlook.com>
2021-03-15 12:11:21 +0100

    Add paginator for show category action

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,18 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Post;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route('/categories/{slug}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
+    public function show(Request $request, PaginatorInterface $paginator, Category $category, ): Response
     {
+        $pagination = $paginator->paginate($category->getPosts(), $request->query->getInt('page', 1), Post::POST_PER_PAGE);
+
         return $this->render('categories/show.html.twig', [
             'category' => $category,
-            'posts' => $category->getPosts(),
+            'pagination' => $pagination,
         ]);
     }
 }
