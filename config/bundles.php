04255ecd6805a2aa73a9b4210455145c7cc85b94
Bastien Ricard <bastien.ricard-dev@outlook.com>
2021-04-15 13:28:06 +0200

    Modify template architecture and add login form

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
